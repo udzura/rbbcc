f4fa581b1e54316c1a84aa6878b7197910d3a3f9
Uchio Kondo <udzura@udzura.jp>
2019-12-21 18:03:21 +0900

    Fix broken extract_arg.rb

--- examples/extract_arg.rb
+++ examples/extract_arg.rb
@@ -5,14 +5,22 @@ include RbBCC
 
 code = <<CLANG
 #include <uapi/linux/ptrace.h>
-int kprobe__sys_execve(struct pt_regs *ctx) {
-  if (!PT_REGS_PARM1(ctx))
-    return 0;
-  char str[80] = {};
-  bpf_probe_read(&str, sizeof(str), (void *)PT_REGS_PARM1(ctx));
-  bpf_trace_printk("execve detected: %s\\n", &str);
+#define ARGSIZE 128
+
+// must be syscall__${the_name} ?
+int syscall__execve(struct pt_regs *ctx,
+    const char __user *filename,
+    const char __user *const __user *__argv,
+    const char __user *const __user *__envp)
+{
+  char buf[ARGSIZE] = {};
+  bpf_probe_read(buf, sizeof(buf), (void *)filename);
+  bpf_trace_printk("execve: %s\\n", &buf);
+
   return 0;
 }
 CLANG
 
-BCC.new(text: code).trace_print
+b = BCC.new(text: code)
+b.attach_kprobe(event: b.get_syscall_fnname("execve"), fn_name: "syscall__execve")
+b.trace_print
