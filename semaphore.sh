303eb33f55c3df00a89f43cca62ed734e04d4d95
Uchio Kondo <udzura@udzura.jp>
2020-02-12 20:40:11 +0900

    Add install process

---
+++ semaphore.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
+echo "deb https://repo.iovisor.org/apt/$(lsb_release -cs) $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/iovisor.list
+sudo apt-get update
+sudo apt-get install libbcc
+
+bundle install --path vendor/bundle
+
+bundle exec ruby -e "require 'rbbcc'; puts RbBCC::VERSION"
