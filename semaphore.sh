7a68f71b730e23447890d3da9656015f400e689a
Uchio Kondo <udzura@udzura.jp>
2020-02-19 20:58:23 +0900

    Fix command name

--- semaphore.sh
+++ semaphore.sh
@@ -2,7 +2,7 @@
 
 # packages
 
-install-packages bison build-essential cmake flex git libedit-dev \
+install-package bison build-essential cmake flex git libedit-dev \
   libllvm6.0 llvm-6.0-dev libclang-6.0-dev python zlib1g-dev libelf-dev
 
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 4052245BD4284CDD
