af9936b7ad8fbde2b953d040adb0c881ca9ab92c
Uchio Kondo <udzura@udzura.jp>
2020-02-19 23:32:35 +0900

    Check each libbcc version

--- semaphore.sh
+++ semaphore.sh
@@ -65,5 +65,9 @@ set -e
 bundle install --path vendor/bundle
 
 bundle exec ruby -e "require 'rbbcc'; puts 'Using rbbcc: %s && libbcc: %s' % [RbBCC::VERSION, RbBCC::Clib.libbcc_version.to_s]"
+if test "$(bundle exec ruby -e 'require %q(rbccc); print RbBCC::Clib.libbcc_version.to_s')" != "${LIBBCC_VERSION}"; then
+  echo "Test target mismatch"
+  exit 127
+fi
 
 sudo -E env PATH=$PATH bundle exec rake test
